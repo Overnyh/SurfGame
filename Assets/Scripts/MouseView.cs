161ada58fea68840820e50ddab7d741cf376e946
WintageOFFC <vavnyhyt@gmail.com>
2023-08-19 21:19:56 +0300

    First commit!

---
+++ Assets/Scripts/MouseView.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseView : MonoBehaviour
+{
+    public float mouseSensityvity = 100f;
+    public Transform playerCamera;
+
+    float xRotation = 0f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensityvity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensityvity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        playerCamera.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX);
+    }
+}
