a7e204accb20e8a0719c9d0c4ac9247d436ff8f7
OverHome <mark.kozirev@gmail.com>
2024-01-24 15:42:23 +0300

    fix x3 case

--- Assets/Scripts/RevardedScript.cs
+++ Assets/Scripts/RevardedScript.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 using YG;
 
@@ -7,17 +8,43 @@ public class RevardedScript : MonoBehaviour
 {
     [SerializeField] private GameObject onOpen;
     [SerializeField] private Case caseScript;
-    
-    private void OnEnable() => YandexGame.RewardVideoEvent += Rewarded;
-    private void OnDisable() => YandexGame.RewardVideoEvent -= Rewarded;
-    
+
+    private bool isGood;
+    private void OnEnable()
+    {
+        YandexGame.RewardVideoEvent += Rewarded;
+        YandexGame.CloseVideoEvent += StartCaseX3;
+        isGood = false;
+    }
+
+    private void OnDisable()
+    {
+        YandexGame.RewardVideoEvent -= Rewarded;
+        YandexGame.CloseVideoEvent -= StartCaseX3;
+    }
+
     void Rewarded(int id)
     {
         if (id == 1)
         {
-            onOpen.SetActive(true);
-            caseScript.CaseOpenX3();
+            isGood = true;
         }
+    }
+
+    void StartCaseX3()
+    {
+        if (isGood)
+        {
+            StartCoroutine(Run());
+        }
+    }
+
+    IEnumerator Run()
+    {
+        yield return new WaitForSeconds(0.2f);
+        isGood = false;
+        onOpen.SetActive(true);
+        caseScript.CaseOpenX3();
     }
     
 }
